518929dc36a38e71dd05114382dfe765551347ca
Syrious <syrious@gmx.net>
2024-12-08 20:16:00 +0100

    Adds button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import BasicApplication from './view/BasicApplication.js';
+import BasicApplication from "./view/BasicApplication.js";
 
-Hooks.once('ready', () => new BasicApplication().render(true, { focus: true }));
+Hooks.on('renderSidebarTab', (app, html) => {
+   console.warn(app)
+   if (game.user.isGM && (app?.id ?? app?.options?.id) === 'actors') {
+      html.find('.thieves-guild-parser').remove();
+
+      const button = $('<button class="thieves-guild-parser">Parse Thieves Guild Actor</button>');
+
+      button.on('click', () => {
+         new BasicApplication().render(true);
+      });
+
+      html.find('.header-actions')
+         .append(button);
+   }
+});
